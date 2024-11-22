2e9021b69854762ce42e37e405a330bb5c96dd53
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-11-22 22:01:05 +0530

    Configure GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,22 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter()
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: false
+        }),
+        paths: {
+            base: dev ? '' : '/future-bits'
+        }
     }
 };
 
