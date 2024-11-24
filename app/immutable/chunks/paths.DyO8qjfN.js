8300da59a7caa2da03fa18b86e6fc3b43302641e
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-11-24 07:50:55 +0530

    Updates

--- app/immutable/chunks/paths.DyO8qjfN.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_157vpfb)==null?void 0:s.base)??"/future-bits";var t;const a=((t=globalThis.__sveltekit_157vpfb)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ app/immutable/chunks/paths.eCcyTGgs.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_190c0kc)==null?void 0:s.base)??"/future-bits";var t;const a=((t=globalThis.__sveltekit_190c0kc)==null?void 0:t.assets)??e;export{a,e as b};
