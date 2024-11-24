d6f3293116988646129431d5a235e92379e74d8e
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-11-24 07:48:23 +0530

    Updates

---
+++ app/immutable/entry/start._NwzAMHf.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D9b43pNK.js";export{t as start};
--- src/svg-icons/icons/QuickReceiptsLogo.svelte
+++
@@ -1,7 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-    <rect x="0" y="0" width="200" height="200" fill="#1E3A8A"/>
-    <rect x="20" y="20" width="160" height="160" fill="none" stroke="#FFA500" stroke-width="6"/>
-    <rect x="0" y="60" width="200" height="60" fill="#1E3A8A"/>
-    <text x="100" y="115" font-family="Georgia, serif" font-size="75" font-weight="bold" text-anchor="middle" fill="white">Quick</text>
-    <text x="100" y="160" font-family="Georgia, serif" font-size="30" font-weight="normal" text-anchor="middle" fill="white">Receipts</text>
-</svg>
