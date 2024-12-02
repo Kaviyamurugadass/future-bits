27d98bd92bf751dcb5b3643af15ac23b37bbf2dc
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-12-02 22:52:36 +0530

    Updates

---
+++ app/immutable/chunks/paths.CXyuCsGa.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_z655tc?.base??"/future-bits",t=globalThis.__sveltekit_z655tc?.assets??s;export{t as a,s as b};
---
+++ app/immutable/entry/start.B9GX4Nb_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CThx3Mc7.js";export{t as start};
