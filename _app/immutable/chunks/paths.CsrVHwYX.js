2b297b936032b1ee4ca1d9d98f692a15f037dd1d
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2024-12-02 22:48:18 +0530

    Updates

---
+++ _app/immutable/chunks/paths.CsrVHwYX.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_15jp7q1)==null?void 0:s.base)??"/future-bits";var t;const e=((t=globalThis.__sveltekit_15jp7q1)==null?void 0:t.assets)??"https://kaviyamurugadass.github.io/future-bits";export{e as a,a as b};
